828f27c3ebe259ac2eac8f34fc988c4db1906ccd
Stella Costa <stella.csouza@ufpe.br>
2026-03-08 23:15:42 -0300

    Add files via upload

---
+++ band_name_generator.py
@@ -0,0 +1,9 @@
+# print("Welcome to the Band Name Generator")
+# city = input("What's the name of the city you grew up in?\n")
+# pet = input("What's your pet's name?\n")
+# print("Your band name is " + city + " " + pet)
+
+print("Welcome to the Band Name Generator")
+city = input("What's the name of the city you grew up in?\n")
+type_of_pet = input("What type of pet do you have?\n")
+print("Your band name could be " + city + " " + type_of_pet)
